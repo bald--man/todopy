c7c5f9ee624a3b1c6ce757aa1fed552e2ab9af05
Oleg Lipovoi <olipovoi@gmail.com>
2016-04-25 22:54:19 +0300

    Added application file.

---
+++ todo.py
@@ -0,0 +1,20 @@
+"""
+
+To do Application
+
+Tkinter GUI Application Development
+""" 
+
+from Tkinter import *
+import ttk
+import sqlite3
+
+
+class ToDo:
+    def __init__(self, master):
+        
+    
+    
+root = Tk()
+application = ToDo(root)
+root.mainloop()
